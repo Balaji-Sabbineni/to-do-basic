24e4082e7284245ef8fa02e8f9801eb06875a8e8
Balaji Sabbineni <balajisabbineni@gmail.com>
2025-03-10 14:09:57 +0530

    add delete functionality

--- script.js
+++ script.js
@@ -12,22 +12,28 @@ function addTask(str) {
     if(str==''){
         return;
     }
+    //create child elements
     let listItem = document.createElement('li');
     let listCheckItem = document.createElement('input');
     listCheckItem.setAttribute('type', 'checkbox');
     let checkLabel = document.createElement('label');
+    let deleteBtn = document.createElement('button');
+    deleteBtn.textContent = 'delete';
 
     let uniqueId = 'checkbox-'+Date.now();
     listCheckItem.setAttribute('id',uniqueId);
     checkLabel.setAttribute('for',uniqueId);
 
     listCheckContent = document.createTextNode(str);
+    //append child elements to parent
     checkLabel.appendChild(listCheckContent);
 
     listItem.appendChild(listCheckItem);
     listItem.appendChild(checkLabel);
-
+    listItem.appendChild(deleteBtn);
     newContent.insertBefore(listItem, newContent.firstChild);
+
+    deleteBtn.onclick = () => {listItem.remove()}
     clearInput();
 }
 
